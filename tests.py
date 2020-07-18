f03bd7d3e65efbc7bc705536cb5b4ef0ae409a19
Diego Magalhães <dmlmagal@gmail.com>
2020-07-18 15:27:49 -0300

    Adds Image conversion endpoint
    This commit adds the second task of the challenge

--- tests.py
+++ tests.py
@@ -1,11 +1,14 @@
 import unittest
 import sqlite3
 from settings import BASE_DIR
-from os import environ, path, remove
+from os import environ, path, remove, system
 from app import application
 from db import db
 import jwt
 import openpyxl
+import tracemalloc
+
+tracemalloc.start()
 
 
 environ['DATABASE'] = 'test.sqlite'
@@ -58,7 +61,7 @@ class XLSXAPITests(unittest.TestCase):
         self.test = application.test_client()
         self.data = {'email': 'user@provider.com'}
         with application.app_context():
-            self.xlsx_data = {'file': open(BASE_DIR+'/sample.xlsx', 'rb')}
+            self.xlsx_data = {'file': open('sample.xlsx', 'rb')}
             db.init_db()
             conn = sqlite3.connect(environ.get('DATABASE'))
             conn.execute('INSERT INTO user (email) VALUES (?)', (self.data['email'],))
@@ -79,7 +82,7 @@ class XLSXAPITests(unittest.TestCase):
 
     def test_api_must_return_200_with_a_valid_token(self):
         token = jwt.encode(self.data, environ.get('SEC_KEY', ''), algorithm='HS256')
-        sample = list((openpyxl.load_workbook(BASE_DIR+'/sample.xlsx').active).values)
+        sample = list((openpyxl.load_workbook('sample.xlsx').active).values)
         sample.pop(0)
         response = self.test.post(
             '/api/excel/info/', headers={'Authorization': f'Bearer {token.decode("UTF-8")}'},
@@ -93,6 +96,47 @@ class XLSXAPITests(unittest.TestCase):
     def tearDown(self):
         self.xlsx_data['file'].close()
         remove(environ['DATABASE'])
+
+
+class ImageApiTests(unittest.TestCase):
+
+    def setUp(self):
+        self.test = application.test_client()
+        self.data = {'email': 'user@provider.com'}
+        self.token = jwt.encode(self.data, environ.get('SEC_KEY', ''), algorithm='HS256')
+        self.params = {'format': 'png', 'file': open('sheetgo.bmp', 'rb')}
+        with application.app_context():
+            db.init_db()
+            conn = sqlite3.connect(environ.get('DATABASE'))
+            conn.execute('INSERT INTO user (email) VALUES (?)', (self.data['email'],))
+            conn.commit()
+
+    def test_image_api_must_return_403_without_authentication_header(self):
+        response = self.test.post('/api/image/convert/')
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.json, {'detail': 'Authentication credentials were not provided.'})
+
+    def test_image_api_must_return_400_if_format_param_is_missing(self):
+        self.params.pop('format')
+        response = self.test.post(
+            '/api/image/convert/', headers={'Authorization': f'Bearer {self.token.decode("UTF-8")}'},
+            content_type='multipart/form-data', data=self.params,
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json, {'detail': 'unsupported format or format is missing.'})
+
+    def test_image_must_be_converted_to_png(self):
+        response = self.test.post(
+            '/api/image/convert/', headers={'Authorization': f'Bearer {self.token.decode("UTF-8")}'},
+            content_type='multipart/form-data', data=self.params,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers.get('Content-Type'), 'image/png')
+        response.close()
+
+    def tearDown(self):
+        self.params['file'].close()
+        system(f'rm -rf {BASE_DIR + "/static/images/converted/*"}')
 
 
 if __name__ == "__main__":
